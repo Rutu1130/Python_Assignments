dc6e23f8bde2b1d7c0e3baca8e7456e9fa22870f
Rutu1130 <154366245+Rutu1130@users.noreply.github.com>
2024-08-01 22:07:22 +0530

    Add files via upload

---
+++ Assignment 2/Assignment2_9.py
@@ -0,0 +1,20 @@
+#Write a program which accept number from user and return number of digitsin that number
+
+
+Num=5187934
+count=0
+while Num!=0:
+    
+    Num = Num//10
+    count = count + 1
+print("Total number of digits:", (count))
+n=(int(input("Enter the number")))
+
+
+
+
+
+
+    
+
+
